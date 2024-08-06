90192b52571be1b222c171ab8cbdb67a8d4230ca
patrick mckay <patrick.mckay@atlasschool.com>
2024-08-06 18:04:19 -0500

    Fixed push in monty.h

--- monty.h
+++ monty.h
@@ -39,7 +39,7 @@ typedef struct instruction_s
 } instruction_t;
 
 /* Function Prototypes */
-void push(stack_t **stack, unsigned int line_number, char *arg);
+void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
 void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number, char *arg);
 void free_stack(stack_t *stack);
